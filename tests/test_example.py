74c3b1f06247509c803048b25943ebed9cf78624
Jamell <jamell@mit.edu>
2017-11-10 11:14:16 -0500

    implement softmax digit classification

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,11 +2,12 @@ from digits import example
 
 import unittest
 
-class SampleTestSuite(unittest.TestCase):
-    """Basic test cases."""
+class SoftmaxTestSuite(unittest.TestCase):
+    """Make sure softmax on MNIST data has the expected accuracy. (0.91 <= accuracy <= 0.93)"""
 
-    def test_example(self):
-        assert example.tensorflow_example() == [[3]]
+    def test_softmax_digits(self):
+        result = example.softmax_digits()
+        assert result >= 0.91 and result <=0.93
 
 
 if __name__ == '__main__':
